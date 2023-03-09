f05fd50b9c6cfd2dabcd993dade915e64dd7b6e4
mxuan0 <mxuan0@gmail.com>
2023-03-09 14:59:37 -0800

    debug

--- experiments/evaluate_after_prune.py
+++ experiments/evaluate_after_prune.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    model = args.checkpoint_path#BERT(output_nums=args.output_num, bert_type=args.bert_type).to(device)
+    model = torch.load(args.checkpoint_path) #BERT(output_nums=args.output_num, bert_type=args.bert_type).to(device)
     # checkpoint = torch.load(args.checkpoint_path)
     # model.load_state_dict(checkpoint['model_state_dict'])
 
